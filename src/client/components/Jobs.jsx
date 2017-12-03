5fa119fef1818fd5632e26a3ec9cd47ccd042d8d
devmetal <devmetal91@gmail.com>
2017-12-03 19:47:36 +0100

    Added items per page control

--- src/client/components/Jobs.jsx
+++ src/client/components/Jobs.jsx
@@ -4,6 +4,8 @@ import { graphql } from 'react-apollo';
 import deepmerge from 'deepmerge';
 import styled from 'styled-components';
 import AppBar from 'material-ui/AppBar';
+import DropdownMenu, { DropDownMenu } from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 import query from '../query/queueJobs';
 import Job from '../components/Job';
 
@@ -22,6 +24,15 @@ class Jobs extends Component {
       queue: PropTypes.instanceOf(Object),
     }).isRequired,
   };
+
+  state = {
+    itemsPerPage: 10,
+  };
+
+  setItemsPerPage = (e, i, val) => {
+    this.setState({ itemsPerPage: val });
+    this.props.loadPage(1, val);
+  }
 
   render() {
     const { data } = this.props;
@@ -50,7 +61,22 @@ class Jobs extends Component {
 
     return (
       <div>
-        <AppBar title={`${hostId}/${name} ${type} jobs`} showMenuIconButton={false} />
+        <AppBar
+          title={`${hostId}/${name} ${type} jobs`} 
+          showMenuIconButton={false}
+        />
+        <div>
+          <DropDownMenu 
+            value={this.state.itemsPerPage} 
+            onChange={this.setItemsPerPage}
+          >
+            <MenuItem value={10} primaryText="10" />
+            <MenuItem value={50} primaryText="50" />
+            <MenuItem value={100} primaryText="100" />
+            <MenuItem value={500} primaryText="500" />
+            <MenuItem value={1000} primaryText="1000" />
+          </DropDownMenu>
+        </div>
         <Flex>
           {jobList.edges.map(job => (
             <Job key={job.id} job={job} />
@@ -73,14 +99,14 @@ export default graphql(query, {
   }),
   props: ({ data, ownProps }) => {
     const { match: { params } } = ownProps;
-    const loadPage = (page) => {
+    const loadPage = (page, itemsPerPage = 10) => {
       const { queueName, hostId, type } = params;
 
       const variables = {
         name: queueName,
         currentPage: page,
-        itemsPerPage: 10,
         state: type,
+        itemsPerPage,
         hostId,
       };
 
